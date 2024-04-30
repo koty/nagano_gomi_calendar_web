07c6d51608a850fa53b037b80d88046213c76800
koty <kouichi.nishizawa+github@gmail.com>
2024-04-30 13:05:27 +0900

    サブフォルダにデプロイできるようにした。

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+/* 公開時のサブディレクトリ */
+const SUB_DIRECTORY = "/nagano_gomi_calendar_web";
+
+/* 本番環境と開発環境の分岐用のフラグ */
+const isProd = process.env.NODE_ENV == "production"
+ 
+const nextConfig = {
+  output: 'export',
+  basePath: isProd ? SUB_DIRECTORY : "",
+};
 
 export default nextConfig;
