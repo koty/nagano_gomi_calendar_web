1bdd9737c4aca42a3514892d044ef9f392414cc7
koty <kouichi.nishizawa+github@gmail.com>
2024-05-06 14:59:52 +0900

    debug

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -2,6 +2,7 @@
 const serviceWorkerInstance = self
 
 setTimeout(() => {
+  console.log('💛 timer callback')
   serviceWorkerInstance.registration.showNotification('通知テスト！', {
     body: '⭐いやーん、テストだよ。'
   })
